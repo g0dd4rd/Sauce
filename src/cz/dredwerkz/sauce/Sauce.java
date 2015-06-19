d85350636b076c5d0b3a6b3122f4d0f0decfd3d4
Jiri Prajzner <jprajzne@redhat.com>
2015-06-19 23:45:33 +0200

    UPD: large refactoring to use WebPage class and TabPane

--- src/cz/dredwerkz/sauce/Sauce.java
+++ src/cz/dredwerkz/sauce/Sauce.java
@@ -2,11 +2,8 @@ package cz.dredwerkz.sauce;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TabPane;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 /**
@@ -16,43 +13,21 @@ import javafx.stage.Stage;
  * @web http://www.dredwerkz.cz
  */
 public class Sauce extends Application {
-
-  String HOME_PAGE = "http://www.seznam.cz";
-
-  private BorderPane bpLayout;
-  private WebEngine we;
   private int HEIGHT = 512;
   private int WIDTH = 800;
 
   @Override
   public void start(Stage stage) {
-    TextField addressBar = new TextField();
-    addressBar.setMinWidth(500);
-    addressBar.setOnAction(a -> {
-      String query = addressBar.getText();
-      if (!(query.startsWith("http") || query.startsWith("https") || query.startsWith("www"))) {
-        we.load("https://duckduckgo.com/?q=" + query);
-      } else if (query.startsWith("www")) {
-        we.load("http://" + query);
-      } else {
-        we.load(query);
+    TabPane tabPane = new TabPane();
+    tabPane.getTabs().add(new WebPage());
+
+    Scene scene = new Scene(tabPane, WIDTH, HEIGHT);
+    scene.setOnKeyPressed(kp -> {
+      if (kp.isControlDown() && kp.getCode() == KeyCode.T) {
+        tabPane.getTabs().add(new WebPage());
+        tabPane.getSelectionModel().selectLast();
       }
-    });;
-
-    WebView wv = new WebView();
-    we = wv.getEngine();
-    we.load(HOME_PAGE);
-
-    bpLayout = new BorderPane();
-    bpLayout.setTop(addressBar);
-    bpLayout.setCenter(wv);
-
-    Scene scene = new Scene(bpLayout, WIDTH, HEIGHT);
-    /*scene.setOnKeyPressed(kp -> {
-     if (kp.isControlDown() && kp.getCode() == KeyCode.T) {
-     // fire up a new tab
-     }
-     });*/
+    });
 
     stage.setTitle("www.dredwerkz.cz - Sauce - @dr3dwerkz");
     stage.setScene(scene);
